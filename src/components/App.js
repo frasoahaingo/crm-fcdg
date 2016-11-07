5d1c0a118c46c8b498792c12f188529f1a59bf06
Francia Rasoahaingo <francia.rasoahaingo@gmail.com>
2016-11-07 23:32:13 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+    render() {
+        return <form>
+        	<input type="text"/>
+        </form>
+    }
+}
